44c09179709ce0315ed6282156073fda25f7dfc8
melmore <melmore@MARVINs-MBP-2.home>
2023-10-08 19:40:58 -0400

    setup middleware, routeserror handlers, and cookie methods

---
+++ portfolio/backend/README.md
@@ -0,0 +1,11 @@
+# Installs
+
+nvm install 16.17.0
+
+yarn add express
+yarn add cors helmet hpp cookie-session compression express-async-errors http-status-code
+
+
+# For TypeScript
+yarn add @types/express
+yarn add @types/cors helmet @types/hpp @types/cookie-session compression express-async-errors http-status-code
---
+++ portfolio/backend/src/setupService.ts
@@ -0,0 +1,64 @@
+import { Application, json, urlencoded, Response, Request, NextFunction } from 'express'
+import http from 'http'
+import cors from 'cors'
+import helmet from 'helmet'
+import hpp from 'hpp'
+import compression from 'compression'
+import cookieSession from 'cookie-session'
+import HTTP_STATUS from 'http-status-codes'
+import 'express-async-errors '
+
+export class AppServer {
+    private app: Application;
+
+    constructor(app: Application) {
+        this.app = app
+    }
+
+    public start(): void {
+        this.securityMiddleware(this.app)
+        this.standardMiddleware(this.app)
+        this.routeMiddleware(this.app)
+        this.globalErrorHandler(this.app)
+        this.standardMiddleware(this.app)
+        this.startServer(this.app)        
+    }
+
+    private securityMiddleware(app: Application): void {
+        app.use(
+            cookieSession({
+                name: 'session',
+                keys: ['test1', 'test2'],
+                maxAge: 24 * 7 * 3600000,
+                secure: false
+            })
+        );
+        app.use(hpp());
+        app.use(helmet());
+        app.use(
+            cors({
+                origin: '*',
+                credentials: true,
+                optionsSuccessStatus: 200,
+                methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+            })
+        );
+    }
+
+    private standardMiddleware(app: Application): void {
+        app.use(compression());
+        app.use(json({ limit: '50mb' }));
+        app.use(urlencoded({ extended:true, limit: '50mb' }))
+    }
+
+    private routeMiddleware(app: Application): void {}
+
+    private globalErrorHandler(app: Application): void {}
+
+    private startServer(app: Application): void {}
+
+    private createSocketIO(httpServer: http.Server): void {}
+
+    private startHttpServer(httpServer: http.Server): void {}
+
+}
